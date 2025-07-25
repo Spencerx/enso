Spencerx/enso

commit 47e66da6772c9dd108506e86de31dfe718509b24

Send all logs to a remote location (#13049)
This change adds appender for a generic remote endpoint. The change extracted common parts between telemetry and general logging capabilities, to avoid code duplication.

# Important Notes
This change removes Sentry appender - it's obsolete.
Removed `telemetry` as a dependency of `runtime-integration-tests` as it was not necessary.

lib/scala/logging-service-logback/src/main/java/org/enso/logging/service/logback/LogbackSetup.java
@@ -12,12 +12,9 @@ import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.helpers.NOPAppender;
 import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
+import ch.qos.logback.core.spi.FilterReply;
 import ch.qos.logback.core.util.Duration;
 import ch.qos.logback.core.util.FileSize;
-import io.sentry.SentryLevel;
-import io.sentry.SentryOptions;
-import io.sentry.SystemOutLogger;
-import io.sentry.logback.SentryAppender;
 import java.io.File;
 import java.net.URI;
 import java.nio.file.Path;
@@ -26,12 +23,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import org.enso.logging.config.Appender;
-import org.enso.logging.config.BaseConfig;
-import org.enso.logging.config.LoggerSetup;
-import org.enso.logging.config.LoggersLevels;
-import org.enso.logging.config.LoggingServiceConfig;
-import org.enso.logging.config.MissingConfigurationField;
+import org.enso.logging.config.*;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
@@ -273,49 +265,14 @@ public final class LogbackSetup extends LoggerSetup {
   }
 
   @Override
-  public boolean setupSentryAppender(Level logLevel, Path logRoot) {
-    // TODO: handle proxy
-    // TODO: shutdown timeout configuration
-    try {
-      LoggerAndContext env = contextInit(logLevel, config, !logToFileEnabled());
-
-      org.enso.logging.config.SentryAppender appenderConfig = config.getSentryAppender();
-      if (appenderConfig == null) {
-        throw new MissingConfigurationField(org.enso.logging.config.SentryAppender.appenderName);
-      }
-      SentryAppender appender = new SentryAppender();
-      SentryOptions opts = new SentryOptions();
-      if (appenderConfig.isDebugEnabled()) {
-        opts.setDebug(true);
-        opts.setLogger(new SystemOutLogger());
-        opts.setDiagnosticLevel(SentryLevel.ERROR);
-      }
-      if (logRoot == null) {
-        opts.setCacheDirPath("sentry");
-      } else {
-        opts.setCacheDirPath(logRoot.resolve(".sentry").toAbsolutePath().toString());
-      }
-      if (appenderConfig.getFlushTimeoutMs() != null) {
-        opts.setFlushTimeoutMillis(appenderConfig.getFlushTimeoutMs());
-      }
-      appender.setMinimumEventLevel(ch.qos.logback.classic.Level.convertAnSLF4JLevel(logLevel));
-      opts.setDsn(appenderConfig.getDsn());
-      appender.setOptions(opts);
-
-      env.finalizeAppender(appender);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      return false;
-    }
-    return true;
-  }
-
-  @Override
-  public boolean setupTelemetryAppender() {
+  public boolean setupTelemetryAppender(URI logsEndpoint, boolean logConnectionFailures) {
     LoggerAndContext env = contextInit(Level.DEBUG, config, false);
-    TelemetryAppender telemetryAppender;
+    AbstractRemoteAppender telemetryAppender;
     try {
-      telemetryAppender = TelemetryAppender.load();
+      telemetryAppender = AbstractRemoteAppender.loadTelemetryAppender();
+      if (telemetryAppender == null) {
+        return false;
+      }
     } catch (Exception e) {
       return false;
     }
@@ -326,9 +283,8 @@ public final class LogbackSetup extends LoggerSetup {
     }
 
     telemetryAppender.setName("telemetry");
-
-    var cloudUri = URI.create(getCloudLogsAPIEndpoint());
-    telemetryAppender.setEndpoint(cloudUri);
+    telemetryAppender.setEndpoint(logsEndpoint);
+    telemetryAppender.setLogConnectionFailures(logConnectionFailures);
 
     // We set-up a thread 'pool' that will contain at most one thread.
     // If the thread is idle for 60 seconds, it will be shut down.
@@ -341,6 +297,44 @@ public final class LogbackSetup extends LoggerSetup {
 
     telemetryAppender.setContext(env.ctx);
     telemetryAppender.start();
+    return true;
+  }
+
+  @Override
+  public boolean setupOpenSearchAppender(
+      Level logLevel, URI logsEndpoint, boolean logConnectionFailures) {
+    LoggerAndContext env = contextInit(logLevel, config, false);
+    AbstractRemoteAppender openSearchAppender;
+    try {
+      openSearchAppender = AbstractRemoteAppender.loadGenericRemoteAppender();
+      if (openSearchAppender == null) {
+        return false;
+      }
+    } catch (Exception e) {
+      return false;
+    }
+    openSearchAppender.setName("engine-remote");
+    openSearchAppender.setEndpoint(logsEndpoint);
+    openSearchAppender.setLogConnectionFailures(logConnectionFailures);
+
+    // We set-up a thread 'pool' that will contain at most one thread.
+    // If the thread is idle for 60 seconds, it will be shut down.
+    var executor = new ThreadPoolExecutor(0, 1, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    openSearchAppender.setExecutor(executor);
+    var filter =
+        new Filter<ILoggingEvent>() {
+          @Override
+          public FilterReply decide(ILoggingEvent event) {
+            var exclude =
+                event.getLoggerName().startsWith("org.enso.telemetry")
+                    || event.getLoggerName().startsWith("org.enso.logging.service");
+            return exclude ? FilterReply.DENY : FilterReply.NEUTRAL;
+          }
+        };
+    filter.setContext(env.ctx);
+    filter.start();
+    openSearchAppender.addFilter(filter);
+    env.finalizeAppender(openSearchAppender);
     return true;
   }
 
@@ -367,10 +361,7 @@ public final class LogbackSetup extends LoggerSetup {
   public void teardown() {
     context().stop();
     var logLevelOnShutdown =
-        config
-            .getLogLevel()
-            .map(name -> Level.valueOf(name.toUpperCase()))
-            .orElseGet(() -> Level.ERROR);
+        config.getLogLevel().map(name -> Level.valueOf(name.toUpperCase())).orElse(Level.ERROR);
     setupConsoleAppender(logLevelOnShutdown);
   }
 

commit 3c3692329588aba6eb96ecb3a7d900a2e39c80f9

SocketAppender always forwards telemetry events (#13645)
There was a regression - no telemetry was sent to the open search server. This PR fixes this regression by ensuring that telemetry events are always accepted and forwarded to the server.

# Important Notes
- I suspect that the regression was caused by #13049, but I was not able to confirm that.

-------------------

- [logging-service/server/log-to-file/log-level in project-manager/application.conf](https://github.com/enso-org/enso/blob/2e9cf92837022f030ccd327109bb6f4d7072c6cf/lib/scala/project-manager/src/main/resources/application.conf#L71) is written into [org.enso.logging.service.LoggingServiceManager#currentLevel](https://github.com/enso-org/enso/blob/566d3d503e35ccf8c363facd590d1332683bce3a/lib/scala/logging-service/src/main/java/org/enso/logging/service/LoggingServiceManager.java#L36) when project-manager is started.
- This field is later used as a value to `--log-level` cmd line option when language server is started from [project-manager](https://github.com/enso-org/enso/blob/2f2747361254aa8892eb8b5c1bd24b10e7eca9b3/lib/scala/project-manager/src/main/scala/org/enso/projectmanager/infrastructure/languageserver/ExecutorWithUnlimitedPool.scala#L125).
- So when set to `debug` (the current default), the language-server is started with `--log-level debug`.
- Which means that `LoggerFactory.get("foo").trace(...)` will not be sent through the socket appender from language-server.
- Note that language server uses a [single socket appender](https://github.com/enso-org/enso/blob/7dcbff420849e00a85e6768616f369e1e03f1589/engine/language-server/src/main/resources/application.conf#L52-L58) to sent the logs to the project-manager's logging server.
- And because all the telemetry logging is done via `trace`, no telemetry events are sent through the socket appender.

------------

lib/scala/logging-service-logback/src/main/java/org/enso/logging/service/logback/LogbackSetup.java
@@ -30,6 +30,7 @@ import org.slf4j.event.Level;
 public final class LogbackSetup extends LoggerSetup {
 
   private static final String CONSOLE_APPENDER_NAME = "enso-console";
+  private static final String TELEMETRY_ROOT_LOGGER = "org.enso.telemetry";
 
   private LogbackSetup(LoggingServiceConfig config, LoggerContext context) {
     this.config = config;
@@ -139,8 +140,26 @@ public final class LogbackSetup extends LoggerSetup {
           Duration.buildByMilliseconds(appenderConfig.getReconnectionDelay()));
     }
 
+    acceptAllTelemetryEvents(socketAppender);
     env.finalizeAppender(socketAppender);
     return true;
+  }
+
+  private static void acceptAllTelemetryEvents(
+      ch.qos.logback.core.Appender<ILoggingEvent> appender) {
+    // This filter lets all the telemetry log events through.
+    var telemetryAcceptingFilter =
+        new Filter<ILoggingEvent>() {
+          @Override
+          public FilterReply decide(ILoggingEvent event) {
+            if (event.getLoggerName().startsWith(TELEMETRY_ROOT_LOGGER)) {
+              return FilterReply.ACCEPT;
+            } else {
+              return FilterReply.NEUTRAL;
+            }
+          }
+        };
+    appender.addFilter(telemetryAcceptingFilter);
   }
 
   @Override
@@ -289,7 +308,7 @@ public final class LogbackSetup extends LoggerSetup {
     var executor = new ThreadPoolExecutor(0, 1, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
     telemetryAppender.setExecutor(executor);
 
-    var telemetryLogger = env.ctx.getLogger("org.enso.telemetry");
+    var telemetryLogger = env.ctx.getLogger(TELEMETRY_ROOT_LOGGER);
     telemetryLogger.addAppender(telemetryAppender);
     telemetryLogger.setLevel(ch.qos.logback.classic.Level.ALL);
 
@@ -324,7 +343,7 @@ public final class LogbackSetup extends LoggerSetup {
           @Override
           public FilterReply decide(ILoggingEvent event) {
             var exclude =
-                event.getLoggerName().startsWith("org.enso.telemetry")
+                event.getLoggerName().startsWith(TELEMETRY_ROOT_LOGGER)
                     || event.getLoggerName().startsWith("org.enso.logging.service");
             return exclude ? FilterReply.DENY : FilterReply.NEUTRAL;
           }