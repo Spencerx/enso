Spencerx/enso

commit 545db47bf074c8faeb24d054f73e37f1111cef29

Remove duplicated IR ASTs for static methods (#13962)
Implements the newly specified behavior of method invocations from #13961.

Co-authored-by: Jaroslav Tulach <jaroslav.tulach@enso.org>

engine/runtime/src/main/java/org/enso/interpreter/runtime/data/Type.java
@@ -223,7 +223,7 @@ public final class Type extends EnsoObject {
     while (at < fill.length) {
       fill[at++] = self;
       if (self.supertype == null) {
-        if (self.builtin) {
+        if (self.builtin && self == ctx.getBuiltins().any()) {
           return at;
         }
         fill[at++] = ctx.getBuiltins().any();
@@ -431,7 +431,7 @@ public final class Type extends EnsoObject {
         @Cached("member") String cachedMember,
         @Cached MethodResolverNode methodResolverNode,
         @Cached("buildSymbol(receiver, member)") UnresolvedSymbol symbol,
-        @Cached("findMethod(eigenType(receiver), symbol, methodResolverNode)") Function func,
+        @Cached("findMethod(receiver, symbol, methodResolverNode)") Function func,
         @Cached("buildInvokeCallableNode(func)") InvokeCallableNode invokeCallableNode)
         throws UnsupportedMessageException, UnsupportedTypeException, ArityException {
       Object[] finalArgs = args;
@@ -457,7 +457,7 @@ public final class Type extends EnsoObject {
             UnknownIdentifierException {
       var symbol = buildSymbol(receiver, member);
       var methodResolverNode = MethodResolverNode.getUncached();
-      var method = findMethod(receiver.getEigentype(), symbol, methodResolverNode);
+      var method = findMethod(receiver, symbol, methodResolverNode);
       if (method == null) {
         throw UnknownIdentifierException.create(member);
       }
@@ -466,17 +466,24 @@ public final class Type extends EnsoObject {
           receiver, member, args, member, methodResolverNode, symbol, method, invokeCallableNode);
     }
 
-    static Type eigenType(Type receiver) {
-      return receiver.getEigentype();
-    }
-
     static UnresolvedSymbol buildSymbol(Type receiver, String member) {
       return UnresolvedSymbol.build(member, receiver.getDefinitionScope());
     }
 
     static Function findMethod(
-        Type receiver, UnresolvedSymbol symbol, MethodResolverNode methodResolverNode) {
-      return InvokeMethodNode.resolveFunction(symbol, receiver, methodResolverNode);
+        Type self, UnresolvedSymbol symbol, MethodResolverNode methodResolverNode) {
+      if (self.isBuiltin()) {
+        // Some builtin types are singletons (have no constructors) and they contain
+        // both instance (methods with self argument) and static methods on themselves.
+        // This makes sense only for builtin types. Which means that here, we try to
+        // find the method on both builtin type and its eigen type.
+        var func = InvokeMethodNode.resolveFunction(symbol, self, self, methodResolverNode);
+        if (func != null) {
+          return func;
+        }
+      }
+      return InvokeMethodNode.resolveFunction(
+          symbol, self, self.getEigentype(), methodResolverNode);
     }
 
     static InvokeCallableNode buildInvokeCallableNode(Function func) {

distribution/lib/Standard/Base/0.0.0-dev/src/Internal/Ordering_Helpers.enso
@@ -25,7 +25,7 @@ type Default_Comparator
        ---
     compare : Any -> Any -> (Ordering|Nothing)
     compare x y =
-        case Any.== x y of
+        case x == y of
             True -> Ordering.Equal
             False ->
                 case Default_Comparator.less_than_builtin x y of

engine/runtime-compiler-dump-igv/src/main/java/org/enso/compiler/dump/igv/EnsoModuleAST.java
@@ -429,8 +429,11 @@ final class EnsoModuleAST {
   private ASTNode buildTree(CallArgument argument) {
     return switch (argument) {
       case CallArgument.Specified specifiedArg -> {
-        Map<String, Object> props = Map.of("argName", specifiedArg.name());
-        var node = newNode(specifiedArg, props);
+        var node = newNode(specifiedArg);
+        if (specifiedArg.name().isDefined()) {
+          var nameNode = buildTree(specifiedArg.name().get());
+          createEdge(node, nameNode, "name");
+        }
         var valueNode = buildTree(specifiedArg.value());
         createEdge(node, valueNode, "value");
         yield node;

engine/runtime-compiler/src/main/java/org/enso/compiler/pass/analyse/types/TypePropagation.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.enso.compiler.MetadataInteropHelpers;
 import org.enso.compiler.common.NameResolutionAlgorithm;
 import org.enso.compiler.core.CompilerError;
+import org.enso.compiler.core.ConstantsNames;
 import org.enso.compiler.core.IR;
 import org.enso.compiler.core.ir.CallArgument;
 import org.enso.compiler.core.ir.Expression;
@@ -355,8 +356,13 @@ abstract class TypePropagation {
       Application.Prefix relatedIR,
       LocalBindingsTyping localBindingsTyping) {
     if (argument.name().isDefined()) {
-      // TODO named arguments are not yet supported
-      return null;
+      var isSelf = ConstantsNames.SELF_ARGUMENT.equals(argument.name().get().name());
+      if (!isSelf) {
+        // TODO named arguments are not yet supported
+        return null;
+      } else {
+        // static invocation syntax is supported
+      }
     }
 
     switch (functionType) {
@@ -370,7 +376,11 @@ abstract class TypePropagation {
 
       case TypeRepresentation.UnresolvedSymbol unresolvedSymbol -> {
         return processUnresolvedSymbolApplication(
-            unresolvedSymbol, argument.value(), localBindingsTyping, relatedIR);
+            unresolvedSymbol,
+            argument.value(),
+            isStaticMethodInvocation(relatedIR),
+            localBindingsTyping,
+            relatedIR);
       }
 
       default -> {
@@ -399,6 +409,7 @@ abstract class TypePropagation {
   private TypeRepresentation processUnresolvedSymbolApplication(
       TypeRepresentation.UnresolvedSymbol function,
       Expression argument,
+      boolean isStaticMethodInvocation,
       LocalBindingsTyping localBindingsTyping,
       IR relatedWholeApplicationIR) {
     var argumentType = tryInferringType(argument, localBindingsTyping);
@@ -421,16 +432,23 @@ abstract class TypePropagation {
             }
           }
 
-          // Then we resolve the _static_ `method` on the `Type` - by looking at the eigen type.
-          // We resolve static calls on the eigen type. It should also contain registrations of the
-          // static variants of member methods, so we don't need to inspect member scope.
-          var staticScope = TypeScopeReference.atomEigenType(typeObject.name());
+          TypeScopeReference staticScope;
+          if (isStaticMethodInvocation) {
+            staticScope = TypeScopeReference.atomType(typeObject.name());
+          } else {
+            staticScope = TypeScopeReference.atomEigenType(typeObject.name());
+          }
           var resolvedStaticMethod = methodTypeResolver.resolveMethod(staticScope, function.name());
           if (resolvedStaticMethod == null) {
             encounteredNoSuchMethod(
                 relatedWholeApplicationIR, argumentType, function.name(), MethodCallKind.STATIC);
           }
-          return resolvedStaticMethod;
+          if (isStaticMethodInvocation) {
+            var withSelf = TypeRepresentation.buildStaticMethod(typeObject, resolvedStaticMethod);
+            return withSelf;
+          } else {
+            return resolvedStaticMethod;
+          }
         }
       }
 
@@ -491,6 +509,23 @@ abstract class TypePropagation {
     assert !name.isEmpty();
     char firstCharacter = name.charAt(0);
     return Character.isUpperCase(firstCharacter);
+  }
+
+  /**
+   * @see <a
+   *     href="https://github.com/enso-org/enso/tree/8c14901627d4d716a67da95d210c7b60f89d30b2/docs/types/dynamic-dispatch.md#static-method-invocation">static
+   *     method invocation specification</a>
+   */
+  private static boolean isStaticMethodInvocation(Application.Prefix applicationIR) {
+    if (applicationIR.arguments().length() >= 2) {
+      var secondArg = applicationIR.arguments().apply(1);
+      return isNamedSelfArgument(secondArg);
+    }
+    return false;
+  }
+
+  private static boolean isNamedSelfArgument(CallArgument arg) {
+    return arg.name().isDefined() && arg.name().get().name().equals(ConstantsNames.SELF_ARGUMENT);
   }
 
   private TypeRepresentation resolveConstructorOnType(

engine/runtime-compiler/src/main/java/org/enso/compiler/pass/analyse/types/TypeRepresentation.java
@@ -39,6 +39,11 @@ public sealed interface TypeRepresentation
     return reversed.stream().reduce(result, (acc, arg) -> new ArrowType(arg, acc));
   }
 
+  static TypeRepresentation buildStaticMethod(
+      TypeRepresentation.TypeObject selfType, TypeRepresentation instanceMethod) {
+    return new ArrowType(selfType.instanceType(), instanceMethod);
+  }
+
   record TopType() implements TypeRepresentation {
     @Override
     public String toString() {

engine/runtime-compiler/src/main/java/org/enso/compiler/pass/lint/NoDuplicateSelfInMethodCalls.java
@@ -1,0 +1,63 @@
+package org.enso.compiler.pass.lint;
+
+import java.util.List;
+import org.enso.compiler.context.InlineContext;
+import org.enso.compiler.context.ModuleContext;
+import org.enso.compiler.core.ConstantsNames;
+import org.enso.compiler.core.ir.CallArgument;
+import org.enso.compiler.core.ir.Expression;
+import org.enso.compiler.core.ir.expression.Application;
+import org.enso.compiler.core.ir.expression.errors.Redefined;
+import org.enso.compiler.pass.IRProcessingPass;
+import org.enso.compiler.pass.MiniIRPass;
+import org.enso.compiler.pass.MiniPassFactory;
+import org.enso.compiler.pass.desugar.GenerateMethodBodies$;
+import scala.collection.immutable.Seq;
+import scala.jdk.javaapi.CollectionConverters;
+
+/** This pass ensures that there are no method calls with two `self` named arguments. */
+public final class NoDuplicateSelfInMethodCalls implements MiniPassFactory {
+  public static final NoDuplicateSelfInMethodCalls INSTANCE = new NoDuplicateSelfInMethodCalls();
+  private static final Mini MINI_INSTANCE = new Mini();
+
+  @Override
+  public Seq<? extends IRProcessingPass> precursorPasses() {
+    java.util.List<IRProcessingPass> list = java.util.List.of(GenerateMethodBodies$.MODULE$);
+    return CollectionConverters.asScala(list).toList();
+  }
+
+  @Override
+  public Seq<? extends IRProcessingPass> invalidatedPasses() {
+    List<IRProcessingPass> empty = List.of();
+    return CollectionConverters.asScala(empty).toList();
+  }
+
+  @Override
+  public MiniIRPass createForModuleCompilation(ModuleContext moduleContext) {
+    return MINI_INSTANCE;
+  }
+
+  @Override
+  public MiniIRPass createForInlineCompilation(InlineContext inlineContext) {
+    return MINI_INSTANCE;
+  }
+
+  private static final class Mini extends MiniIRPass {
+
+    @Override
+    public Expression transformExpression(Expression expr) {
+      if (expr instanceof Application.Prefix application) {
+        var namedSelfArguments = application.arguments().filter(Mini::isNamedSelfArgument);
+        if (namedSelfArguments.length() > 1) {
+          return new Redefined.SelfArg(application.identifiedLocation(), application.passData());
+        }
+      }
+      return expr;
+    }
+
+    private static boolean isNamedSelfArgument(CallArgument callArg) {
+      return callArg.name().isDefined()
+          && callArg.name().get().name().equals(ConstantsNames.SELF_ARGUMENT);
+    }
+  }
+}

engine/runtime-compiler/src/main/java/org/enso/compiler/pass/resolve/MethodDefinitions.java
@@ -14,7 +14,6 @@ import org.enso.compiler.core.ir.Module;
 import org.enso.compiler.core.ir.Name;
 import org.enso.compiler.core.ir.expression.errors.Conversion;
 import org.enso.compiler.core.ir.expression.errors.Conversion.UnsupportedSourceType$;
-import org.enso.compiler.core.ir.module.scope.Definition;
 import org.enso.compiler.core.ir.module.scope.definition.Method;
 import org.enso.compiler.data.BindingsMap;
 import org.enso.compiler.data.BindingsMap.Resolution;
@@ -123,8 +122,10 @@ public final class MethodDefinitions implements MiniPassFactory {
                       return switch (method) {
                         case Method.Explicit explicitMethod -> {
                           var isStatic = computeIsStatic(explicitMethod.body());
+                          var methodWithAscribedSelf =
+                              addTypeAscriptionToSelfParameter(explicitMethod);
                           var resolvedMethod =
-                              explicitMethod
+                              methodWithAscribedSelf
                                   .copyBuilder()
                                   .methodReference(resolvedMethodRef)
                                   .isStatic(isStatic)
@@ -159,99 +160,51 @@ public final class MethodDefinitions implements MiniPassFactory {
                     }
                   });
 
-      java.util.List<Definition> withStaticAliases = new ArrayList<>();
-      for (var def : CollectionConverters.asJava(newDefs)) {
-        withStaticAliases.add(def);
-        if (def instanceof Method.Explicit method && !method.isStatic()) {
-          var staticAlias = generateStaticAliasMethod(method);
-          if (staticAlias != null) {
-            withStaticAliases.add(staticAlias);
-          }
-        }
-      }
-
-      return moduleIr.copyWithBindings(CollectionConverters.asScala(withStaticAliases).toList());
+      return moduleIr.copyWithBindings(newDefs);
     }
 
-    /**
-     * Returns null if there is no suitable static alias method that can be generated for the given
-     * {@code method}.
-     *
-     * @param method Non-static method from which a static alias method is generated.
-     * @return Static alias method for the given {@code method} or null.
-     */
-    private Method.Explicit generateStaticAliasMethod(Method.Explicit method) {
-      assert !method.isStatic();
+    private Method.Explicit addTypeAscriptionToSelfParameter(Method.Explicit method) {
       var typePointer = method.methodReference().typePointer();
       if (typePointer.isEmpty()) {
-        return null;
+        return method;
       }
       var resolution =
           MetadataInteropHelpers.getMetadataOrNull(typePointer.get(), INSTANCE, Resolution.class);
       if (resolution == null) {
-        return null;
+        return method;
       }
       if (resolution.target() instanceof ResolvedType resType
-          && canGenerateStaticWrappers(resType.tp())) {
-        assert method.body() instanceof Function.Lambda;
-        var dup = method.duplicate(true, true, true, false);
-        // This is the self argument that will receive the `SelfType.type` value upon dispatch, it
-        // is
-        // added to avoid modifying the dispatch mechanism.
-        var syntheticModuleSelfArg =
-            DefinitionArgument.Specified.builder()
-                .name(new Name.Self(null, true, new MetadataStorage()))
-                .suspended(false)
-                .build();
+          && method.body() instanceof Function.Lambda body
+          && canAddSelfParameterTypeAscription(resType.tp())) {
+        var bodyDup = body.duplicate(true, true, true, false);
         // Here we add the type ascription ensuring that the 'proper' self argument only
         // accepts _instances_ of the type (or triggers conversions)
-        var newBodyRef =
-            Persistance.Reference.of(addTypeAscriptionToSelfArgument(dup.body()), true);
-        var newBody =
-            Function.Lambda.builder()
-                .arguments(
-                    // This is the synthetic Self argument that gets the static module
-                    list(syntheticModuleSelfArg))
-                .bodyReference(newBodyRef)
-                .canBeTCO(true)
-                .build();
-        // The actual `self` argument that is referenced inside of method body is the second one in
-        // the lambda.
-        // This is the argument that will hold the actual instance of the object we are calling on,
-        // e.g. `My_Type.method instance`.
-        // We add a type check to it to ensure only `instance` of `My_Type` can be passed to it.
-        var staticMethod =
-            dup.copyBuilder()
-                .bodyReference(Persistance.Reference.of(newBody))
-                .isStatic(true)
-                .isStaticWrapperForInstanceMethod(true)
-                .build();
-        return staticMethod;
+        var newBodyRef = Persistance.Reference.of(addTypeAscriptionToSelfParameter(bodyDup), true);
+        return method.copyBuilder().bodyReference(newBodyRef).build();
       }
-      return null;
+      return method;
     }
 
-    private static Expression addTypeAscriptionToSelfArgument(Expression methodBody) {
-      if (methodBody instanceof Function.Lambda lambda) {
-        if (lambda.arguments().isEmpty()) {
-          throw new CompilerError(
-              "MethodDefinitions pass: expected at least one argument (self) in the method, but got"
-                  + " none.");
-        }
-        var firstArg = lambda.arguments().head();
-        if (firstArg instanceof DefinitionArgument.Specified selfArg
-            && selfArg.name() instanceof Name.Self) {
-          var selfType = new Name.SelfType(selfArg.identifiedLocation(), new MetadataStorage());
-          var newSelfArg = selfArg.copyWithAscribedType(Option.apply(selfType));
-          return lambdaWithNewSelfArg(lambda, newSelfArg);
-        } else {
-          throw new CompilerError(
-              "MethodDefinitions pass: expected the first argument to be `self`, but got "
-                  + firstArg);
-        }
+    private static boolean canAddSelfParameterTypeAscription(Type tp) {
+      return tp.members().nonEmpty() || (tp.builtinType() && !"Nothing".equals(tp.name()));
+    }
+
+    private static Expression addTypeAscriptionToSelfParameter(Function.Lambda lambda) {
+      if (lambda.arguments().isEmpty()) {
+        throw new CompilerError(
+            "MethodDefinitions pass: expected at least one argument (self) in the method, but got"
+                + " none.");
+      }
+      var firstArg = lambda.arguments().head();
+      if (firstArg instanceof DefinitionArgument.Specified selfArg
+          && selfArg.name() instanceof Name.Self) {
+        var selfType = new Name.SelfType(selfArg.identifiedLocation(), new MetadataStorage());
+        var newSelfArg = selfArg.copyWithAscribedType(Option.apply(selfType));
+        return lambdaWithNewSelfArg(lambda, newSelfArg);
       } else {
         throw new CompilerError(
-            "Unexpected body type " + methodBody + " in MethodDefinitions pass.");
+            "MethodDefinitions pass: expected the first argument to be `self`, but got "
+                + firstArg);
       }
     }
 
@@ -262,15 +215,6 @@ public final class MethodDefinitions implements MiniPassFactory {
       args.set(0, newSelfArg);
       var newArgs = CollectionConverters.asScala(args).toList();
       return lambda.copyWithArguments(newArgs);
-    }
-
-    // Generate static wrappers for
-    // 1. Types having at least one type constructor
-    // 2. All builtin types except for Nothing. Nothing's eigentype is Nothing and not Nothing.type,
-    //    would lead to overriding conflicts.
-    //    TODO: Remove the hardcoded type once Enso's annotations can define parameters.
-    private static boolean canGenerateStaticWrappers(Type tp) {
-      return tp.members().nonEmpty() || (tp.builtinType() && !"Nothing".equals(tp.name()));
     }
 
     private Name resolveType(Name typePointer, BindingsMap availableSymbolsMap) {

engine/runtime-compiler/src/main/scala/org/enso/compiler/Passes.scala
@@ -12,6 +12,7 @@ import org.enso.compiler.pass.desugar._
 import org.enso.compiler.pass.lint.unusedimports.UnusedImports
 import org.enso.compiler.pass.lint.{
   ModuleNameConflicts,
+  NoDuplicateSelfInMethodCalls,
   NoSelfInStatic,
   ShadowedPatternFields,
   UnusedBindings
@@ -103,7 +104,11 @@ class Passes(config: CompilerConfig) {
     ) ++ (if (config.isLintingDisabled) {
             Nil
           } else {
-            List(UnusedBindings, NoSelfInStatic)
+            List(
+              UnusedBindings,
+              NoSelfInStatic,
+              NoDuplicateSelfInMethodCalls.INSTANCE
+            )
           }) ++ (if (config.staticAnalysisEnabled) {
                    List(
                      TypeInferenceSignatures.INSTANCE,

commit d611d99113777e063649bc756fb29a75f6a419a3

Fix inconsistent polyglot method invocation via type (#14334)
Fixes inconsistency introduced in #13962 - static method invocation does not work in interop.

engine/runtime/src/main/java/org/enso/interpreter/runtime/data/Type.java
@@ -5,11 +5,9 @@ import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnknownIdentifierException;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
-import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
@@ -28,10 +26,8 @@ import org.enso.interpreter.node.callable.InvokeCallableNode;
 import org.enso.interpreter.node.callable.InvokeCallableNode.ArgumentsExecutionMode;
 import org.enso.interpreter.node.callable.InvokeCallableNode.DefaultsExecutionMode;
 import org.enso.interpreter.node.callable.InvokeMethodNode;
-import org.enso.interpreter.node.callable.resolver.MethodResolverNode;
 import org.enso.interpreter.runtime.EnsoContext;
 import org.enso.interpreter.runtime.ModuleScopeBuilder;
-import org.enso.interpreter.runtime.callable.UnresolvedSymbol;
 import org.enso.interpreter.runtime.callable.argument.ArgumentDefinition;
 import org.enso.interpreter.runtime.callable.argument.CallArgumentInfo;
 import org.enso.interpreter.runtime.callable.function.Function;
@@ -429,11 +425,8 @@ public final class Type extends EnsoObject {
         String member,
         Object[] args,
         @Cached("member") String cachedMember,
-        @Cached MethodResolverNode methodResolverNode,
-        @Cached("buildSymbol(receiver, member)") UnresolvedSymbol symbol,
-        @Cached("findMethod(receiver, symbol, methodResolverNode)") Function func,
-        @Cached("buildInvokeCallableNode(func)") InvokeCallableNode invokeCallableNode)
-        throws UnsupportedMessageException, UnsupportedTypeException, ArityException {
+        @Cached("findMethod(receiver, cachedMember)") Function func,
+        @Cached("buildInvokeCallableNode(func)") InvokeCallableNode invokeCallableNode) {
       Object[] finalArgs = args;
       if (InvokeMethodNode.shouldPrependSyntheticSelfArg(func.getSchema(), args.length)) {
         var argsWithReceiver = new Object[args.length + 1];
@@ -446,44 +439,18 @@ public final class Type extends EnsoObject {
 
     @Specialization(replaces = "doCached")
     @TruffleBoundary
-    static Object doUncached(
-        Type receiver,
-        String member,
-        Object[] args,
-        @CachedLibrary(limit = "3") InteropLibrary interop)
-        throws UnsupportedMessageException,
-            UnsupportedTypeException,
-            ArityException,
-            UnknownIdentifierException {
-      var symbol = buildSymbol(receiver, member);
-      var methodResolverNode = MethodResolverNode.getUncached();
-      var method = findMethod(receiver, symbol, methodResolverNode);
+    static Object doUncached(Type receiver, String member, Object[] args)
+        throws UnknownIdentifierException {
+      var method = findMethod(receiver, member);
       if (method == null) {
         throw UnknownIdentifierException.create(member);
       }
       var invokeCallableNode = buildInvokeCallableNode(method);
-      return doCached(
-          receiver, member, args, member, methodResolverNode, symbol, method, invokeCallableNode);
+      return doCached(receiver, member, args, member, method, invokeCallableNode);
     }
 
-    static UnresolvedSymbol buildSymbol(Type receiver, String member) {
-      return UnresolvedSymbol.build(member, receiver.getDefinitionScope());
-    }
-
-    static Function findMethod(
-        Type self, UnresolvedSymbol symbol, MethodResolverNode methodResolverNode) {
-      if (self.isBuiltin()) {
-        // Some builtin types are singletons (have no constructors) and they contain
-        // both instance (methods with self argument) and static methods on themselves.
-        // This makes sense only for builtin types. Which means that here, we try to
-        // find the method on both builtin type and its eigen type.
-        var func = InvokeMethodNode.resolveFunction(symbol, self, self, methodResolverNode);
-        if (func != null) {
-          return func;
-        }
-      }
-      return InvokeMethodNode.resolveFunction(
-          symbol, self, self.getEigentype(), methodResolverNode);
+    static Function findMethod(Type self, String methodName) {
+      return self.methods().get(methodName);
     }
 
     static InvokeCallableNode buildInvokeCallableNode(Function func) {

engine/runtime-integration-tests/src/test/java/org/enso/interpreter/test/interop/AtomInteropTest.java
@@ -477,6 +477,51 @@ public class AtomInteropTest {
   }
 
   @Test
+  public void instanceMethod_CanBeInvokedViaAtom() {
+    var atom =
+        ctxRule.evalModule(
+            """
+            type My_Type
+                Cons
+                method self = 42
+            main = My_Type.Cons
+            """);
+    assertThat(atom.hasMember("method"), is(true));
+    assertThat(atom.canInvokeMember("method"), is(true));
+    var res = atom.invokeMember("method");
+    assertThat(res.asInt(), is(42));
+  }
+
+  @Test
+  public void instanceMethod_IsMemberOfType() {
+    var type =
+        ctxRule.evalModule(
+            """
+            type My_Type
+                Cons
+                method self = 42
+            main = My_Type
+            """);
+    assertThat(type.hasMember("method"), is(true));
+    assertThat(type.canInvokeMember("method"), is(true));
+  }
+
+  @Test
+  public void instanceMethod_CanBeInvokedViaType() {
+    var atom =
+        ctxRule.evalModule(
+            """
+            type My_Type
+                Cons
+                method self = 42
+            main = My_Type.Cons
+            """);
+    var type = atom.getMetaObject();
+    var res = type.invokeMember("method", atom);
+    assertThat(res.asInt(), is(42));
+  }
+
+  @Test
   public void invokeVsReadAndExecute() throws Exception {
     var atom =
         ctxRule.evalModule(